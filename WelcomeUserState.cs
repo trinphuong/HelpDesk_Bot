99ac4ad18e17d0ac04bb5634ea4c0f3ecbdb183c
フォン チン <trin.phuong@noc-net.co.jp>
2019-09-03 18:18:12 +0900

    add welcome for new user

---
+++ WelcomeUserState.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.BotBuilderSamples
+{
+    // Stores User Welcome state for the conversation.
+    // Stored in "Microsoft.Bot.Builder.ConversationState" and
+    // backed by "Microsoft.Bot.Builder.MemoryStorage".
+
+    public class WelcomeUserState
+    {
+        // Gets or sets whether the user has been welcomed in the conversation.
+        public bool DidBotWelcomeUser { get; set; } = false;
+    }
+}
